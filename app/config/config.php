d3eee61380e042df7f8c86ba3a39dee153732116
alinosi <146401086+alinosi@users.noreply.github.com>
2025-04-06 16:04:25 +0700

    Update config.php
    update url

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php 
     // define menyimpan ('NAMA DEFINISI' , 'YANG DIDEFINISIKAN')
-    define('BASEURL', 'http://localhost/PHP/TUGASGDG/public');
+    define('BASEURL', 'http://localhost/PHP/marketplace/public');
 
     //db
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
-    define('DB_PASS', 'S@suke12');
+    define('DB_PASS', '');
     define('DB_NAME', 'marketplace');

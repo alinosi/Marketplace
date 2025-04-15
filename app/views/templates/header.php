28ed07cee630d3537dac0868cfaa981b503de609
alinosi <146401086+alinosi@users.noreply.github.com>
2025-04-15 13:31:58 +0700

    Refactor header navigation to include background color and remove unused home carousel

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -123,7 +123,7 @@
     </style>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
+<nav class="navbar navbar-expand-lg bg-body-tertiary bg-warning sticky-top">
   <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
